560bdd2a6442b756c851246b883a7335101e64cc
André <andreluiz.brodfilho@gmail.com>
2026-09-10 20:30:44 -0300

    Comentario

--- classe_cominit.py
+++ classe_cominit.py
@@ -7,5 +7,6 @@ class Equipamento:
         print(f"Nome: {self.nome}")
         print(f"Patrimônio: {self.patrimonio}")
 
+#classe com instanciacao
 notebook=Equipamento("Notebook Dell","PAT-001")
 notebook.exibir_dados()
