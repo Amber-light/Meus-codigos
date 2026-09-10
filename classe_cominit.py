71666b393f4a1211c8895d045954c52f9234b9ce
André <andreluiz.brodfilho@gmail.com>
2026-09-10 20:25:26 -0300

    atualização

---
+++ classe_cominit.py
@@ -0,0 +1,11 @@
+class Equipamento:
+    def __init__(self,nome,patrimonio):
+        self.nome=nome
+        self.patrimonio=patrimonio
+
+    def exibir_dados(self):
+        print(f"Nome: {self.nome}")
+        print(f"Patrimônio: {self.patrimonio}")
+
+notebook=Equipamento("Notebook Dell","PAT-001")
+notebook.exibir_dados()
